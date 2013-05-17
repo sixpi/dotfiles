382c157b249918757e8c4773ab86f2b35518cc14
Bing Xia <aix.bing@gmail.com>
2013-05-17 15:54:57 -0300

    use emacs/vim instead of sublime text

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,8 @@
-export EDITOR='atom'
+HAVE_EMACS=$(command -v emacs)
+HAVE_VIM=$(command -v vim)
+
+# set editor, prefer emacs if we have it
+EDITOR=vi
+test -n "$HAVE_VIM" && EDITOR=vim
+test -n "$HAVE_EMACS" && EDITOR=emacs
+export EDITOR
