cf1a4bdee014139919c1990994847c0e63e7da8b
Bing Xia <aix.bing@gmail.com>
2014-07-03 08:58:54 -0400

    Add /usr/local/lib to LD_LIBRARY_PATH.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
 export PATH="./bin:${HOME}/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-
+export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
