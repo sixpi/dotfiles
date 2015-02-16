c21d329086d21d03bc2cc1f0b3bd839f4e27128f
Bing Xia <aix.bing@gmail.com>
2015-02-16 16:48:55 -0500

    Update PATH preferences.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,25 @@
-export PATH="./bin:${HOME}/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
+pathmunge()
+{
+    if ! echo $PATH | /bin/grep -qE "(^|:)$1($|:)" ; then
+        if [ "$2" = "after" ] ; then
+            PATH=$PATH:$1
+        else
+            PATH=$1:$PATH
+        fi
+    fi
+}
+
+pathmunge $ZSH/bin
+pathmunge $HOME/bin
+
+if [[ -d $HOME/anaconda/bin ]]
+then
+    pathmunge $HOME/anaconda/bin
+fi
+
+pathmunge ./bin
+
+export PATH
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+unset pathmunge
