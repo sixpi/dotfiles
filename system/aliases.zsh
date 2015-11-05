e349469505380fb92a4531367efa6cf908fd599d
Bing Xia <aix.bing@gmail.com>
2015-11-05 09:58:57 -0500

    Update aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,7 +1,7 @@
-alias ls="ls -F --color"
-alias l="ls -lAh --color"
-alias ll="ls -l --color"
-alias la='ls -A --color'
+alias ls="ls -F --color=auto"
+alias l="ls -lAh --color=auto"
+alias ll="ls -l --color=auto"
+alias la='ls -A --color=auto'
 
 alias ack='ack-grep'
 
@@ -12,3 +12,5 @@ alias clipb='xclip -i -selection clipboard'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
+
+alias ssh_scc="ssh -t scc '/usr3/graduate/sixpi/bin/zsh'"
